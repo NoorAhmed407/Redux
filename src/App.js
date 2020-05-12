bfda6d9f9b55bce1c7fff756642017738077dc26
noorahmed407 <engrnoorahmed407@gmail.com>
2020-05-12 04:15:54 -0700

    Routes Added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import Header from './Components/header';
 import Home from './Components/home';
 import About from './Components/about';
+// import Service from './Components/service';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Header />
-      <Home />
-      <About />
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/about" component={About}/>
+          {/* <Route path="/service" component={Service}/> */}
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
