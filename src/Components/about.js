f5e999e7deee729e83dff073f5dd16dc0d5b5caa
noorahmed407 <engrnoorahmed407@gmail.com>
2020-05-12 10:38:05 -0700

    About Completed

--- src/Components/about.js
+++ src/Components/about.js
@@ -6,13 +6,13 @@ import {connect} from 'react-redux'
 const Cards = (props) =>{
     return(
         <div className="col-md-4">
-            <div class="card border-dark mb-3">
-                <div class="card-header">
+            <div className="card border-dark mb-3">
+                <div className="card-header">
                     <img alt="cardpic" src={`https://joeschmoe.io/api/v1/${props.cardname}`} />
                 </div>
-                <div class="card-body text-dark">
-                    <h2 class="card-title">{props.cardname}</h2>
-                    <h5 class="card-text">{props.cardDesignation}</h5>
+                <div className="card-body text-center text-dark">
+                    <h2 className="card-title">{props.cardname}</h2>
+                    <h5 className="card-text">{props.cardDesignation}</h5>
                 </div>
             </div>
         </div>
@@ -20,29 +20,27 @@ const Cards = (props) =>{
 }
 
 
-    const getCards = (props) => {
-        const getcard = props.data.map(getCardData=>{
-            return(
-            <Cards
-            key={getCardData.id}
-            cardname={getCardData.name}
-            cardDesignation={getCardData.Designation}
-            />
-            )
-        });
 
-        return getcard;
-    }
 
 class About extends React.Component{
+
     render(){
         return(
             <div>
-            
                  <h1 className="text-center mt-5">About Us</h1>
-
                  <div className="row">
-                    {this.getCards}
+                    {
+                        this.props.data.map(getCardData=>{
+                        
+                            return(
+                                <Cards
+                                key={getCardData.id}
+                                cardname={getCardData.name}
+                                cardDesignation={getCardData.Designation}           
+                            />
+                            ) 
+                        })
+                    }
                  </div>
                 
             </div>
